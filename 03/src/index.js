4e5d810389ee698f2aac41cb5fd6879d700245df
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-06-19 02:20:17 +0530

    Rendering File

--- 03/src/index.js
+++ 03/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import Data from "./Data";
+import Form from "./Form";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Data/>
+    <Form/>
   </React.StrictMode>
 );
 
