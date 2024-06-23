c12e7bc3f49e2e79970e9d1ed297655c4a0bc9f0
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-06-23 21:34:22 +0530

    RTK-Query and CRUD

--- 05/src/App.js
+++ 05/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { useGetApiByNameQuery } from './services/api';
+import { useCreatePostMutation, useGetApiByNameQuery, useDeletePostMutation, useUpdatePostMutation } from './services/api';
 
 const App = () => {
-  const { data, error, isLoading, isSuccess, isFetching } = useGetApiByNameQuery();
+  const { data, error, isLoading, isSuccess, isFetching, refetch } = useGetApiByNameQuery();
 
   return (
     <div>
@@ -17,10 +17,52 @@ const App = () => {
           ))}
         </ul>
       )}
+      <div>
+        <AddPost refetch={refetch} />
+      </div>
     </div>
   );
 };
 
+const AddPost = ({ refetch }) => {
+  const [createPost] = useCreatePostMutation();
+  const [deletePost] = useDeletePostMutation();
+  const [updatePost] = useUpdatePostMutation();
+
+  const post = {
+    id: 2,
+    title: "Posted",
+    author: "Aniket"
+  };
+
+  const postUpdate = {
+    id: 2,
+    title: "Updated",
+    author: "momento"
+  };
+
+  const handlePost = async () => {
+    await createPost(post);
+    refetch();
+  };
+
+  const handleDelete = async () => {
+    await deletePost(post.id);
+    refetch();
+  };
+
+  const handleUpdate = async () => {
+    await updatePost(postUpdate);
+    refetch();
+  };
+
+  return (
+    <>
+      <button onClick={handlePost}>Add Post</button>
+      <button onClick={handleDelete}>Delete Post</button>
+      <button onClick={handleUpdate}>Update Post</button>
+    </>
+  );
+};
+
 export default App;
-
-
--- 05/src/services/api.jsx
+++ 05/src/services/api.jsx
@@ -5,12 +5,41 @@ export const api = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: 'http://localhost:3000/',
     }),
+    tagTypes: ['Post'], // Add tag types
     endpoints: (builder) => ({
         getApiByName: builder.query({
             query: () => '/posts',
+            providesTags: ['Post'], // Provide tags
+        }),
+        createPost: builder.mutation({
+            query: (newPost) => ({
+                url: '/posts',
+                method: 'POST',
+                body: newPost,
+            }),
+            invalidatesTags: ['Post'], // Invalidate tags
+        }),
+        deletePost: builder.mutation({
+            query: (id) => ({
+                url: `/posts/${id}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: ['Post'], // Invalidate tags
+        }),
+        updatePost: builder.mutation({
+            query: (updatedPost) => ({
+                url: `/posts/${updatedPost.id}`,
+                method: 'PUT',
+                body: updatedPost,
+            }),
+            invalidatesTags: ['Post'], // Invalidate tags
         }),
     }),
 });
 
-export const { useGetApiByNameQuery } = api;
-
+export const {
+    useGetApiByNameQuery,
+    useCreatePostMutation,
+    useDeletePostMutation,
+    useUpdatePostMutation,
+} = api;
